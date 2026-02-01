04ab9cd661b0500f502f1fbeda0b0662c592ec7b
mithlesh <kamaldewangan367@gmail.com>
2026-02-01 11:21:22 +0530

    create intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Intro from './components/intro'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Intro/>
+    </>
   )
 }
 
---
+++ src/components/Intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='bg-black text-white w-screen h-screen'>Intro</div>
+  )
+}
+
+export default Intro
