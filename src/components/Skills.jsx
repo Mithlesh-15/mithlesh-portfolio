85c51b318774d03cd026a3115319b812c47a90cf
mithlesh <kamaldewangan367@gmail.com>
2026-02-15 07:24:24 +0530

    add heading in skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Skills() {
   return (
-    <div className="text-white text-3xl font-bold py-20 px-10">
-    
-    <div>Skills</div>
-    <div>Skills</div>
-    <div>Skills</div>
-    
-    </div>
-  )
+    <section
+      id="skills"
+      className="w-full px-4 sm:px-6 lg:px-10 py-20 text-white"
+    >
+      <div className="mx-auto max-w-6xl">
+        {/* Heading */}
+        <h2 className="text-center text-3xl sm:text-4xl lg:text-5xl font-semibold mb-12">
+          Skills & Technologies
+        </h2>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
