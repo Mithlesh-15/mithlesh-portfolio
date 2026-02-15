badf98c6a5257fa830bf973a77c9e8bf66f7df56
mithlesh <kamaldewangan367@gmail.com>
2026-02-15 07:33:50 +0530

    enhance skill heading

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,9 @@ function Skills() {
       id="skills"
       className="w-full px-4 sm:px-6 lg:px-10 py-20 text-white"
     >
-      <div className="mx-auto max-w-6xl">
+      <div className="flex justify-center mx-auto max-w-6xl">
         {/* Heading */}
-        <h2 className="text-center text-3xl sm:text-4xl lg:text-5xl font-semibold mb-12">
+        <h2 className=" text-3xl sm:text-4xl lg:text-5xl font-semibold mb-12 inline-block border-b-4 pb-2">
           Skills & Technologies
         </h2>
       </div>
