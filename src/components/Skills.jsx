b74ca739d39eae0cf0618629877f606a98dbc1ef
mithlesh <kamaldewangan367@gmail.com>
2026-02-17 18:22:59 +0530

    add three skills line

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,7 +19,43 @@ function Skills() {
           Skills & Technologies
         </h2>
       </div>
-      <div className="w-full flex justify-center px-4">
+      <div className="w-full flex justify-center flex-col px-4 gap-10">
+        <div className="text-black flex flex-wrap justify-center gap-6">
+          {line1.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+            >
+              <img
+                src="/components/about/photo.jpeg"
+                alt={`${item.name} logo`}
+                className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
+              />
+              <span className="font-medium text-xl">{item.name}</span>
+            </a>
+          ))}
+        </div>
+        <div className="text-black flex flex-wrap justify-center gap-6">
+          {line1.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+            >
+              <img
+                src="/components/about/photo.jpeg"
+                alt={`${item.name} logo`}
+                className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
+              />
+              <span className="font-medium text-xl">{item.name}</span>
+            </a>
+          ))}
+        </div>
         <div className="text-black flex flex-wrap justify-center gap-6">
           {line1.map((item, index) => (
             <a
