8d440f116145a604bed655a378ea137e810336e0
mithlesh <kamaldewangan367@gmail.com>
2026-02-17 18:32:44 +0530

    fix overflow problem

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 function Skills() {
-  const line1 = [
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-    { name: "React", link: "https://react.dev" },
-  ];
+  const line1 = [];
   return (
     <section id="skills" className="w-full py-20 text-white">
       <div className="flex justify-center mx-auto max-w-6xl">
@@ -20,7 +11,7 @@ function Skills() {
         </h2>
       </div>
       <div className="w-full flex justify-center flex-col px-4 gap-10">
-        <div className="text-black flex flex-wrap justify-center gap-6">
+        <div className="text-black flex justify-center gap-6 overflow-hidden">
           {line1.map((item, index) => (
             <a
               key={index}
@@ -38,7 +29,7 @@ function Skills() {
             </a>
           ))}
         </div>
-        <div className="text-black flex flex-wrap justify-center gap-6">
+        <div className="text-black flex justify-center gap-6 overflow-hidden">
           {line1.map((item, index) => (
             <a
               key={index}
@@ -56,7 +47,25 @@ function Skills() {
             </a>
           ))}
         </div>
-        <div className="text-black flex flex-wrap justify-center gap-6">
+        <div className="text-black flex justify-center gap-6 overflow-hidden">
+          {line1.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+            >
+              <img
+                src="/components/about/photo.jpeg"
+                alt={`${item.name} logo`}
+                className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
+              />
+              <span className="font-medium text-xl">{item.name}</span>
+            </a>
+          ))}
+        </div>
+        <div className="text-black flex justify-center gap-6 overflow-hidden">
           {line1.map((item, index) => (
             <a
               key={index}
