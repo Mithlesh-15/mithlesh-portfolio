5ea6443072ae7b698f91e6cd412102ef211b0b58
mithlesh <kamaldewangan367@gmail.com>
2026-02-17 18:16:18 +0530

    build structure of skill layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 
 function Skills() {
+  const line1 = [{ name: "React" }, { name: "React" }, { name: "React" }, { name: "React" }, { name: "React" }];
   return (
-    <section
-      id="skills"
-      className="w-full px-4 sm:px-6 lg:px-10 py-20 text-white"
-    >
+    <section id="skills" className="w-full py-20 text-white">
       <div className="flex justify-center mx-auto max-w-6xl">
         {/* Heading */}
         <h2 className=" text-3xl sm:text-4xl lg:text-5xl font-semibold mb-12 inline-block border-b-4 pb-2">
           Skills & Technologies
         </h2>
+      </div>
+      <div className="w-full flex justify-center px-4">
+        <div className="text-black flex flex-wrap justify-center gap-6">
+          {line1.map((item, index) => (
+            <div key={index} className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm">
+              <img
+                src="/components/about/photo.jpeg"
+                alt={`${item.name} logo`}
+                className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
+              />
+              <span className="font-medium text-xl">{item.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
