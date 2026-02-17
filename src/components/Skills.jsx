d37e701e8f58de8ec0953d947ba4d18118a902f2
mithlesh <kamaldewangan367@gmail.com>
2026-02-17 19:21:33 +0530

    add elementa for line1 skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,85 +1,155 @@
 import React from "react";
 
 function Skills() {
-  const line1 = [];
+  const line1 = [
+    {
+      name: "HTML",
+      logo: "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
+      link: "https://www.w3schools.com/html/",
+    },
+
+    {
+      name: "CSS",
+      logo: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+      link: "https://www.w3schools.com/css/",
+    },
+
+    {
+      name: "JavaScript",
+      logo: "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+      link: "https://www.w3schools.com/js/",
+    },
+
+    {
+      name: "Typescript",
+      logo: "https://img.icons8.com/?size=100&id=wpZmKzk11AzJ&format=png&color=000000",
+      link: "https://www.typescriptlang.org/",
+    },
+
+    {
+      name: "React",
+      logo: "https://img.icons8.com/?size=100&id=NfbyHexzVEDk&format=png&color=000000",
+      link: "https://react.dev/",
+    },
+
+    {
+      name: "Node.js",
+      logo: "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000",
+      link: "https://nodejs.org/en",
+    },
+
+    {
+      name: "Express.js",
+      logo: "https://img.icons8.com/?size=100&id=SDVmtZ6VBGXt&format=png&color=000000",
+      link: "https://expressjs.com/",
+    },
+
+    // repect element for infinite scroll
+
+    {
+      name: "HTML",
+      logo: "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
+      link: "https://www.w3schools.com/html/",
+    },
+
+    {
+      name: "CSS",
+      logo: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+      link: "https://www.w3schools.com/css/",
+    },
+
+    {
+      name: "JavaScript",
+      logo: "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+      link: "https://www.w3schools.com/js/",
+    },
+
+    {
+      name: "Typescript",
+      logo: "https://img.icons8.com/?size=100&id=wpZmKzk11AzJ&format=png&color=000000",
+      link: "https://www.typescriptlang.org/",
+    },
+
+    {
+      name: "React",
+      logo: "https://img.icons8.com/?size=100&id=NfbyHexzVEDk&format=png&color=000000",
+      link: "https://react.dev/",
+    },
+
+    {
+      name: "Node.js",
+      logo: "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000",
+      link: "https://nodejs.org/en",
+    },
+
+    {
+      name: "Express.js",
+      logo: "https://img.icons8.com/?size=100&id=SDVmtZ6VBGXt&format=png&color=000000",
+      link: "https://expressjs.com/",
+    },
+  ];
+  
+  const line3 = [];
   return (
-    <section id="skills" className="w-full py-20 text-white">
+    <section id="skills" className="w-full py-20 text-white ">
       <div className="flex justify-center mx-auto max-w-6xl">
         {/* Heading */}
         <h2 className=" text-3xl sm:text-4xl lg:text-5xl font-semibold mb-12 inline-block border-b-4 pb-2">
           Skills & Technologies
         </h2>
       </div>
-      <div className="w-full flex justify-center flex-col px-4 gap-10">
-        <div className="text-black flex justify-center gap-6 overflow-hidden">
+      <div className="w-full flex justify-center flex-col gap-10">
+        <div className="w-full text-black flex justify-center gap-6 overflow-x-hidden">
           {line1.map((item, index) => (
             <a
               key={index}
               href={item.link}
               target="_blank"
               rel="noreferrer"
-              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110 shrink-0"
             >
               <img
-                src="/components/about/photo.jpeg"
+                src={item.logo}
                 alt={`${item.name} logo`}
                 className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
               />
-              <span className="font-medium text-xl">{item.name}</span>
+              <span className="font-bold text-xl">{item.name}</span>
             </a>
           ))}
         </div>
-        <div className="text-black flex justify-center gap-6 overflow-hidden">
-          {line1.map((item, index) => (
+        <div className="w-full text-black flex justify-center gap-6 overflow-x-hidden">
+          {line2.map((item, index) => (
             <a
               key={index}
               href={item.link}
               target="_blank"
               rel="noreferrer"
-              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110 shrink-0"
             >
               <img
-                src="/components/about/photo.jpeg"
+                src={item.logo}
                 alt={`${item.name} logo`}
                 className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
               />
-              <span className="font-medium text-xl">{item.name}</span>
+              <span className="font-bold text-xl">{item.name}</span>
             </a>
           ))}
         </div>
-        <div className="text-black flex justify-center gap-6 overflow-hidden">
-          {line1.map((item, index) => (
+        <div className="w-full text-black flex justify-center gap-6 overflow-x-hidden">
+          {line3.map((item, index) => (
             <a
               key={index}
               href={item.link}
               target="_blank"
               rel="noreferrer"
-              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
+              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110 shrink-0"
             >
               <img
-                src="/components/about/photo.jpeg"
+                src={item.logo}
                 alt={`${item.name} logo`}
                 className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
               />
-              <span className="font-medium text-xl">{item.name}</span>
-            </a>
-          ))}
-        </div>
-        <div className="text-black flex justify-center gap-6 overflow-hidden">
-          {line1.map((item, index) => (
-            <a
-              key={index}
-              href={item.link}
-              target="_blank"
-              rel="noreferrer"
-              className="bg-white rounded-lg px-8 py-3 flex items-center gap-3 shadow-sm transition-transform duration-200 hover:scale-110"
-            >
-              <img
-                src="/components/about/photo.jpeg"
-                alt={`${item.name} logo`}
-                className="w-8 h-8 sm:w-10 sm:h-10 object-contain "
-              />
-              <span className="font-medium text-xl">{item.name}</span>
+              <span className="font-bold text-xl">{item.name}</span>
             </a>
           ))}
         </div>
